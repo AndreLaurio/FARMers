1193ede342c0a298b069ac5684cf9b40cab119db
Hyperstorm321 <45072321+Hyperstorm321@users.noreply.github.com>
2020-08-19 13:06:36 -0800

    First upload

---
+++ farmers-frontend/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Index from '../views/Index.vue'
+// import Admin from '../views/admin/AdminHome'
+// import Seller from '../views/seller/SellerHome'
+// import Buyer from '../views/buyer/BuyerHome'
+// import AdminApproval from '../views/admin/AdminApproval'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Index',
+    component: Index
+  },
+  // {
+  //   path: '/seller',
+  //   name: 'Seller',
+  //   component: Seller
+  // },
+  // {
+  //   path: '/buyer',
+  //   name: 'Buyer',
+  //   component: Buyer
+  // },
+  // {
+  //   path: '/admin',
+  //   name: 'Admin',
+  //   component: Admin
+  // },
+  // {
+  //   path:'/admin/approval',
+  //   name: 'AdminApproval',
+  //   component: AdminApproval
+  // }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
