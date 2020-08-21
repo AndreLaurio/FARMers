5e291197c09f01ae372afce0bf561ed04af02f09
Joshua Mamangun <mamangunjoshua@gmail.com>
2020-08-21 08:48:47 +0800

    Update StocksController.php

--- farmers-backend/app/Http/Controllers/StocksController.php
+++ farmers-backend/app/Http/Controllers/StocksController.php
@@ -11,12 +11,12 @@ class StocksController extends Controller
         $query = DB::select(DB::raw(
                 'SELECT CONCAT(
                     DATE_FORMAT(s.date_start, "%M %Y"),
-                    "-",
+                    " - ",
                     DATE_FORMAT(s.date_end, "%M %Y")
                   ) AS month,
                   se.season,
                   CONCAT(s.value_start, " - ", s.value_end) AS value,
-                  LOWER(CONCAT(s.value_start, " - ", s.value_end, " per ", me.measurement)) AS value_with_measurement
+                  LOWER(CONCAT(s.value_start, " per ", me.measurement)) AS value_with_measurement
                  FROM stocks AS s
                  LEFT JOIN seasons AS se ON se.season_id = s.season_id
                  LEFT JOIN measurements AS me ON me.measurement_id = s.measurement_id
